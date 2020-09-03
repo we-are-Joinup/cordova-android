eb9a77e2f821ba4a40b0a3f3f99131ad9a1bdad2
lempere <lempere@lempere.com>
2020-09-03 17:46:31 +0200

    Allow minSdk 19 Revert merge bump to 22 and drop pre-Lollipop: https://github.com/apache/cordova-android/pull/915/files

--- framework/src/org/apache/cordova/engine/SystemWebViewClient.java
+++ framework/src/org/apache/cordova/engine/SystemWebViewClient.java
@@ -113,6 +113,7 @@ public class SystemWebViewClient extends WebViewClient {
      * @param request
      */
     @Override
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public void onReceivedClientCertRequest (WebView view, ClientCertRequest request)
     {
 
